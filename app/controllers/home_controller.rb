d02049b65dd513232338f77514c196e4c44de1d2
Jafer Daltro <jafer.daltro@gmail.com>
2022-07-08 22:39:34 -0300

    add home#index

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  # GET /: Retornar uma mensagem "REST Back-end Challenge 20201209 Running"
+  def index
+    render json: { message: 'REST Back-end Challenge 20201209 Running' }
+  end
+end
